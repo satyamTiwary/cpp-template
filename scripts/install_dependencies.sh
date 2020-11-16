4f667c1055681576fedccf2b21fe2f03fa0134c8
Satyam tiwary <satyamtiwary@vvbiotech.com>
2020-11-16 17:26:25 +0530

    minor CI NOT

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,11 @@
 sudo apt install build-essential clang-11 clang-tidy ccache 
 
 # Cpp
-sudo apt-get install python3-pip cmake cppcheck doxygen graphviz
+sudo apt-get install mosquitto python3 python3-pip cmake cppcheck 
+sudo python3 -m pip install conan
+
 pip3 install setuptools
 pip3 install wheel
 pip3 install conan clang-format
+
+sudo apt-get install doxygen graphviz
