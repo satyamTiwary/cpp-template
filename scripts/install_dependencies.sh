74fe1ed696663177678d8d02b29e491f12bc6b42
Satyam tiwary <satyamtiwary@vvbiotech.com>
2020-11-16 12:54:47 +0530

    minor refactor - cmake files without understanding

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,14 @@
+sudo apt-get update
 sudo apt install build-essential
-bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
-pip install --user conan
-sudo apt-get install cmake
+sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" 
+sudo apt-get install python3-pip
 
-
-sudo apt-get install doxygen
-sudo apt-get install graphviz
 sudo apt-get install cmake
 sudo apt-get install cppcheck
+sudo apt-get install doxygen
+sudo apt-get install graphviz
+
+sudo apt install clang-tidy
+
+pip3 install --user conan
+pip3 install clang-format
