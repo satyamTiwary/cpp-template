e4ddcef09660ee3935c10f514614c94370e99502
Satyam tiwary <satyamtiwary@vvbiotech.com>
2020-11-16 17:03:15 +0530

    minor trying to fix CI

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,18 @@
+# Essential
 sudo apt-get update
 sudo apt install build-essential
 sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" 
 sudo apt-get install python3-pip
 
+# Cpp
 sudo apt-get install cmake
 sudo apt-get install cppcheck
-sudo apt-get install doxygen
-sudo apt-get install graphviz
 
 sudo apt install clang-tidy
 
 pip3 install --user conan
 pip3 install clang-format
+
+# Optional
+sudo apt-get install doxygen
+sudo apt-get install graphviz
---
+++ test/Driver/gpio.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch.hpp>
+#include <iostream>
+
+int f() { return 1; }
+
+TEST_CASE("gpio can be constructed.")
+{
+//   gpio g;
+//   std::cout << g.str();
+//   REQUIRE(f() == 1);
+}
