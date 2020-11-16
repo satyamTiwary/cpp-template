dd76685ed73671b4e802522ef414bcc3bdb6d6a0
Satyam tiwary <satyamtiwary@vvbiotech.com>
2020-11-16 17:09:36 +0530

    still fixing

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,18 +1,13 @@
 # Essential
-sudo apt-get update
+# sudo apt-get update
 sudo apt install build-essential
 sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" 
 sudo apt-get install python3-pip
 
 # Cpp
-sudo apt-get install cmake
-sudo apt-get install cppcheck
-
-sudo apt install clang-tidy
-
-pip3 install --user conan
-pip3 install clang-format
+sudo apt-get install cmake cppcheck
+sudo apt install clang-tidy ccache
+pip3 install conan clang-format
 
 # Optional
-sudo apt-get install doxygen
-sudo apt-get install graphviz
+sudo apt-get install doxygen graphviz
